aeef35ecc86abeb1a47a90e799a33b044410803c
Jortizuwu <joseher2511@gmail.com>
2024-08-07 11:15:58 -0500

    match discovery server create and match server

---
+++ match/src/test/java/com/typefigth/match/MatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typefigth.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
