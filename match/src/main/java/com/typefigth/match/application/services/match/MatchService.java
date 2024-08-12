016ffda5026dd0da592f5f06ed75f8270d2723ca
Jortizuwu <joseher2511@gmail.com>
2024-08-12 16:33:22 -0500

    add service for cancel and finish match

--- match/src/main/java/com/typefigth/match/application/services/match/MatchService.java
+++ match/src/main/java/com/typefigth/match/application/services/match/MatchService.java
@@ -1,17 +1,14 @@
 package com.typefigth.match.application.services.match;
 
 import com.typefigth.match.domain.models.Match;
-import com.typefigth.match.domain.ports.in.match.AssignOpponentToMatchUseCase;
-import com.typefigth.match.domain.ports.in.match.CreateMatchUseCase;
-import com.typefigth.match.domain.ports.in.match.GetMatchUseCase;
-import com.typefigth.match.domain.ports.in.match.ListMatchUseCase;
+import com.typefigth.match.domain.ports.in.match.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class MatchService implements CreateMatchUseCase, GetMatchUseCase, ListMatchUseCase, AssignOpponentToMatchUseCase {
+public class MatchService implements CreateMatchUseCase, GetMatchUseCase, ListMatchUseCase, AssignOpponentToMatchUseCase, CancelMatchUseCase, FinishMatchUseCase {
 
     private final CreateMatchUseCase createMatchUseCase;
 
@@ -21,11 +18,24 @@ public class MatchService implements CreateMatchUseCase, GetMatchUseCase, ListMa
 
     private final AssignOpponentToMatchUseCase assignOpponentToMatchUseCase;
 
-    public MatchService(CreateMatchUseCase createMatchUseCase, GetMatchUseCase getMatchUseCase, ListMatchUseCase listMatchUseCase, AssignOpponentToMatchUseCase assignOpponentToMatchUseCase) {
+    private final CancelMatchUseCase cancelMatchUseCase;
+
+    private final FinishMatchUseCase finishMatchUseCase;
+
+    public MatchService(
+            CreateMatchUseCase createMatchUseCase,
+            GetMatchUseCase getMatchUseCase,
+            ListMatchUseCase listMatchUseCase,
+            AssignOpponentToMatchUseCase assignOpponentToMatchUseCase,
+            CancelMatchUseCase cancelMatchUseCase,
+            FinishMatchUseCase finishMatchUseCase
+    ) {
         this.createMatchUseCase = createMatchUseCase;
         this.getMatchUseCase = getMatchUseCase;
         this.listMatchUseCase = listMatchUseCase;
         this.assignOpponentToMatchUseCase = assignOpponentToMatchUseCase;
+        this.cancelMatchUseCase = cancelMatchUseCase;
+        this.finishMatchUseCase = finishMatchUseCase;
     }
 
     @Override
@@ -45,7 +55,17 @@ public class MatchService implements CreateMatchUseCase, GetMatchUseCase, ListMa
 
     @Override
     public Match assignOpponentToMatch(Match match, String opponentId) {
-       return this.assignOpponentToMatchUseCase.assignOpponentToMatch(match, opponentId);
+        return this.assignOpponentToMatchUseCase.assignOpponentToMatch(match, opponentId);
+    }
+
+    @Override
+    public Match cancelMatch(Match match) {
+        return this.cancelMatchUseCase.cancelMatch(match);
+    }
+
+    @Override
+    public Match finishMatch(Match match) {
+        return this.finishMatchUseCase.finishMatch(match);
     }
 }
 
